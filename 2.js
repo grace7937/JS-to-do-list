07956715b1d442e80c1a379a35df771eb03c91b0
grace7937 <66059987+grace7937@users.noreply.github.com>
2021-02-07 23:42:45 +0900

    2021-02-07

--- 2.js
+++ 2.js
@@ -5,3 +5,14 @@ checkBox.type = 'checkbox';
     checkBox.addEventListener('change', () =>
       this.changeTodoStatusHandler(todo.id).bind(this)
     );
+
+    function humanReadable(sec) {
+        const h = sec % 3600
+        
+        // 초가 입력 되면초를 시간으로 나누고 값받고
+        // 분을 초로환산한 60을 나누고
+        // 그나머지는 그냥 반환
+          console.log(h)
+       }
+      
+       humanReadable(3600);
